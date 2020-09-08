19d56f61bff2d0f8f298e2a26c28a73fc3c9292f
Aitor Simona Bouzas <simonaaitor@gmail.com>
2020-09-08 12:48:32 +0200

    idle tag, initial scripts created, playing idle on loop when in play mode
    next some running and motion matching test

---
+++ C.W.L.F/Assets/BipedLocomotion/Biped.cs
@@ -0,0 +1,33 @@
+﻿using Unity.Kinematica;
+using UnityEngine;
+
+namespace CWLF
+{
+    [RequireComponent(typeof(Kinematica))]
+    public class Biped : MonoBehaviour
+    {
+        bool idle;
+
+        private void OnEnable()
+        {
+            var kinematica = GetComponent<Kinematica>();
+            ref var synthesizer = ref kinematica.Synthesizer.Ref;
+            synthesizer.PlayFirstSequence(synthesizer.Query.Where(IdleTrait.Trait));
+        }
+
+        private void Update()
+        {
+            //ref var synthesizer = ref kinematica.Synthesizer.Ref;
+
+            //if(Input.anyKeyDown)
+            //{
+            //    idle ^= true;
+
+            //    if(idle)
+            //    {
+            //        synthesizer.PlayFirstSequence(synthesizer.Query.Where("IdleTag",Locomotio))
+            //    }
+            //}
+        }
+    }
+}
---
+++ C.W.L.F/Assets/BipedLocomotion/Tags/IdleTag.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Unity.Kinematica;
+using Unity.Kinematica.Editor;
+
+[Trait]
+public struct IdleTrait
+{
+    public static IdleTrait Trait => new IdleTrait();
+}
+
+[Serializable]
+[Tag("IdleTag", "#4850d2")]
+internal struct IdleTag : Payload<IdleTrait>
+{
+    public static IdleTag CreateDefaultTag()
+    {
+        return new IdleTag();
+    }
+
+    public IdleTrait Build(PayloadBuilder builder)
+    {
+        return IdleTrait.Trait;
+    }
+}

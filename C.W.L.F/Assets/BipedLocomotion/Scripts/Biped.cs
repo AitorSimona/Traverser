fe5459ef6bb0bec8922fe073f731327fa6bd9035
Aitor Simona Bouzas <simonaaitor@gmail.com>
2020-09-08 14:16:51 +0200

    preparing first locomotion
    many things in kinematica changed, sample readme is targeting an old version! bipedloc sample will be my next objective (along basic controls, gamepad preferred)

---
+++ C.W.L.F/Assets/BipedLocomotion/Scripts/Biped.cs
@@ -0,0 +1,35 @@
+﻿using Unity.Kinematica;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace CWLF
+{
+    [RequireComponent(typeof(Kinematica))]
+    public class Biped : MonoBehaviour
+    {
+       
+        //bool idle;
+
+        private void OnEnable()
+        {
+            var kinematica = GetComponent<Kinematica>();
+            ref MotionSynthesizer synthesizer = ref kinematica.Synthesizer.Ref;
+            synthesizer.PlayFirstSequence(synthesizer.Query.Where(IdleTrait.Trait));
+        }
+
+        private void Update()
+        {
+            //ref var synthesizer = ref kinematica.Synthesizer.Ref;
+
+            //if(Input.anyKeyDown)
+            //{
+            //    idle ^= true;
+
+            //    if(idle)
+            //    {
+            //        synthesizer.PlayFirstSequence(synthesizer.Query.Where("IdleTag",Locomotio))
+            //    }
+            //}
+        }
+    }
+}

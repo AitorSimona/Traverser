b80c5ee79108fe3da6932f3cf855cc6afda8d3e4
Aitor Simona Bouzas <simonaaitor@gmail.com>
2020-09-10 13:06:01 +0200

    basic locomotion implemented
    using kinematica's input (to be changed?), understanding biped locomotion sample, kinematica basic stuff, jobs (multithreading)

--- C.W.L.F/Assets/BipedLocomotion/Scripts/Biped.cs
+++ C.W.L.F/Assets/BipedLocomotion/Scripts/Biped.cs
@@ -1,35 +1,139 @@
-﻿using Unity.Kinematica;
+﻿using UnityEngine;
+using Unity.Kinematica;
 using Unity.Mathematics;
-using UnityEngine;
+using Unity.Burst;
+using Unity.Jobs;
+using Unity.Collections;
+using System;
+using Unity.SnapshotDebugger;
 
 namespace CWLF
 {
-    [RequireComponent(typeof(Kinematica))]
-    public class Biped : MonoBehaviour
+    [BurstCompile(CompileSynchronously = true)] //Burst is primarily designed to work efficiently with the Job system.
+    public struct KinematicaJob : IJob // multhithreaded code, pose prediction will be executed faster
     {
-       
-        //bool idle;
+        public MemoryRef<MotionSynthesizer> synthesizer;
 
-        private void OnEnable()
+        public PoseSet idleCandidates;
+
+        public PoseSet locomotionCandidates;
+
+        public Trajectory trajectory; // struct that holds a trajectory (array of transforms)
+
+        public bool idle;
+
+        ref MotionSynthesizer Synthesizer => ref synthesizer.Ref; // reference to the synthesizer
+
+        public void Execute()
         {
-            var kinematica = GetComponent<Kinematica>();
+            if (idle && Synthesizer.MatchPose(idleCandidates, Synthesizer.Time, MatchOptions.DontMatchIfCandidateIsPlaying | MatchOptions.LoopSegment, 0.01f))
+            {
+                return;
+            }
+
+            Synthesizer.MatchPoseAndTrajectory(locomotionCandidates, Synthesizer.Time, trajectory);
+        }
+    }
+
+
+    [RequireComponent(typeof(Kinematica))]
+    public class Biped : SnapshotProvider // Derived from MonoBehaviour, allows the use of kinematica's snapshot debugger
+    {
+        // --- Inspector variables ---
+        [Header("Prediction settings")]
+        [Tooltip("Desired speed in meters per second for slow movement.")]
+        [Range(0.0f, 10.0f)]
+        public float desiredSpeedSlow = 3.0f;
+
+        [Tooltip("Desired speed in meters per second for fast movement.")]
+        [Range(0.0f, 10.0f)]
+        public float desiredSpeedFast = 5.5f;
+
+        [Tooltip("How fast or slow the target velocity is supposed to be reached.")]
+        [Range(0.0f, 1.0f)]
+        public float velocityPercentage = 1.0f;
+
+        [Tooltip("How fast or slow the desired forward direction is supposed to be reached.")]
+        [Range(0.0f, 1.0f)]
+        public float forwardPercentage = 1.0f;
+
+        // --- Internal variables ---
+        Kinematica kinematica;
+
+        PoseSet idleCandidates;
+        PoseSet locomotionCandidates;
+        Trajectory trajectory;
+
+        [Snapshot]
+        float3 movementDirection = Missing.forward;
+
+        [Snapshot]
+        float moveIntensity = 0.0f;
+
+        float desiredLinearSpeed => InputUtility.IsPressingActionButton() ? desiredSpeedFast : desiredSpeedSlow;
+
+        // --- Methods ---
+        public override void OnEnable()
+        {
+            base.OnEnable();
+            kinematica = GetComponent<Kinematica>();
             ref MotionSynthesizer synthesizer = ref kinematica.Synthesizer.Ref;
-            synthesizer.PlayFirstSequence(synthesizer.Query.Where(IdleTrait.Trait));
+
+            // --- Initialize arrays ---
+            idleCandidates = synthesizer.Query.Where("Idle", Locomotion.Default).And(Idle.Default);
+            locomotionCandidates = synthesizer.Query.Where("Locomotion", Locomotion.Default).Except(Idle.Default);
+            trajectory = synthesizer.CreateTrajectory(Allocator.Persistent);
+
+            // --- Play default animation ---
+            synthesizer.PlayFirstSequence(idleCandidates);
         }
 
-        private void Update()
+        public override void OnDisable()
         {
-            //ref var synthesizer = ref kinematica.Synthesizer.Ref;
+            base.OnDisable();
 
-            //if(Input.anyKeyDown)
-            //{
-            //    idle ^= true;
+            // --- Delete arrays ---
+            idleCandidates.Dispose();
+            locomotionCandidates.Dispose();
+            trajectory.Dispose();
+        }
 
-            //    if(idle)
-            //    {
-            //        synthesizer.PlayFirstSequence(synthesizer.Query.Where("IdleTag",Locomotio))
-            //    }
-            //}
+        public override void OnEarlyUpdate(bool rewind)
+        {
+            base.OnEarlyUpdate(rewind);
+
+            Utility.GetInputMove(ref movementDirection, ref moveIntensity);
+        }
+
+        void Update()
+        {
+            float desiredSpeed = moveIntensity * desiredLinearSpeed;
+
+            // --- Create final trajectory from given parameters ---
+            TrajectoryPrediction.CreateFromDirection(ref kinematica.Synthesizer.Ref,
+                movementDirection,
+                desiredSpeed,
+                trajectory,
+                velocityPercentage,
+                forwardPercentage).Generate();
+
+            // --- Set up a job so pose prediction runs faster (multithreading) ---
+            KinematicaJob job = new KinematicaJob()
+            {
+                synthesizer = kinematica.Synthesizer,
+                idleCandidates = idleCandidates,
+                locomotionCandidates = locomotionCandidates,
+                trajectory = trajectory,
+                idle = moveIntensity == 0.0f
+            };
+
+            // --- Finally tell kinematica to wait for this job to finish before executing other stuff ---
+            kinematica.AddJobDependency(job.Schedule());
+        }
+
+        private void OnGUI()
+        {
+            InputUtility.DisplayMissingInputs(InputUtility.ActionButtonInput | InputUtility.MoveInput | InputUtility.CameraInput);
         }
     }
 }
---
+++ C.W.L.F/Assets/BipedLocomotion/Tags/Editor/LocomotionTag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Unity.Kinematica.Editor;
+
+namespace CWLF
+{
+    [Serializable]
+    [Tag("Locomotion", "#4850d2")]
+    public struct LocomotionTag : Payload<Locomotion>
+    {
+        public Locomotion Build(PayloadBuilder builder)
+        {
+            return Locomotion.Default;
+        }
+    }
+}

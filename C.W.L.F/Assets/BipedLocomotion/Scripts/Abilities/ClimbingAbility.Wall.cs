42581e1ade87ef4887d50df35617cf373c6cd6db
Aitor Simona Bouzas <simonaaitor@gmail.com>
2020-09-22 20:16:31 +0200

    added anchor definition and main getters
    still need to go through some functions, I need to visualize what is happening before being able to atomize everything

--- C.W.L.F/Assets/BipedLocomotion/Scripts/Abilities/ClimbingAbility.Wall.cs
+++ C.W.L.F/Assets/BipedLocomotion/Scripts/Abilities/ClimbingAbility.Wall.cs
@@ -8,10 +8,23 @@ using UnityEngine;
 namespace CWLF
 {
 
-    // --- Definition of wall geometry, so we can adapt motion when colliding into it ---
-
     public partial class ClimbingAbility : SnapshotProvider, Ability
     {
+        // --- Definition of a wall's anchor, the point we are attached to --- 
+        public struct WallAnchor
+        {
+            // --- Attributes that define an anchor point ---
+            public float x;
+            public float y;
+
+            // --- Construction ---
+            public static WallAnchor Create()
+            {
+                return new WallAnchor();
+            }
+        }
+
+        // --- Definition of wall geometry, so we can adapt motion when colliding into it ---
         public struct WallGeometry
         {
             // --- Attributes that define a wall object ---
@@ -64,7 +77,160 @@ namespace CWLF
                 return new AffineTransform(transform.position, transform.rotation);
             }
 
-        }
+            public static float PlaneDistance(float3 normal, float3 up, float3 position)
+            {
+                float3 orthogonal = math.cross(normal, up);
+
+                // u*V1 + v*V2, where u and v = 1.0f
+                float3 vertex = normal + up + orthogonal;
+
+                float d = -math.dot(normal, vertex);
+
+                return math.dot(normal, position) + d;
+            }
+
+            public float3 WorldToLocal(float3 p)
+            {
+                return Missing.mul(inverseTransformPoint(p) - center, Missing.recip(size)) * 2.0f;
+            }
+
+            // --- Getters ---
+            public static float3 GetNormal(int index)
+            {
+                // simple getter of 4 normal directions
+                float3[] normals = new float3[]
+                {
+                     Missing.right,
+                     -Missing.right,
+                     Missing.forward,
+                     -Missing.forward
+                };
+
+                return normals[index];
+            }
+
+            public float3 GetNormalLocalSpace()
+            {
+                return normal;
+            }
+
+            public float3 GetNormalWorldSpace()
+            {
+                // local to world
+                return transformDirection(GetNormalLocalSpace());
+            }
+
+            public float3 GetOrthogonalLocalSpace()
+            {
+                // return the perpendicular vector to the normal
+                return math.cross(normal, Missing.up);
+            }
+
+            public float3 GetOrthogonalWorldSpace()
+            {
+                // local to world
+                return transformDirection(GetOrthogonalLocalSpace());
+            }
+
+            public float3 GetBaseVertex()
+            {
+                // u*V1 + v*V2, where u and v = 1.0f
+                return normal + Missing.up + GetOrthogonalLocalSpace();
+            }
+
+            public static float3 GetNextVertexCCW(float3 normal, float3 vertex)
+            {
+                // https://math.stackexchange.com/questions/304700
+                return normal + math.cross(normal, vertex);
+            }
+
+            public float GetWidth()
+            {
+                // get world width
+                float3 orthogonal = GetOrthogonalLocalSpace();
+
+                return math.dot(orthogonal, size) * math.dot(orthogonal, scale);
+            }
+
+            public float GetHeight()
+            {
+                // get world height
+                return size.y * scale.y;
+            }
+
+            public float GetHeight(ref WallAnchor anchor)
+            {
+                return GetHeight() * (1.0f - anchor.y);
+            }
+
+            public float3 GetPosition(WallAnchor anchor)
+            {
+                float3 orthogonal = GetOrthogonalLocalSpace();
+                float3 vertex = GetBaseVertex();
+
+                float3 v0 = transformPoint(center + Missing.mul(size, vertex) * 0.5f);
+                float3 o = transformDirection(orthogonal);
+
+                float u = GetWidth() * anchor.x;
+                float v = GetHeight() * anchor.y;
+
+                return v0 - (o * u) - (Missing.up * v);
+            }
+
+            // --- Anchor ---
+            public WallAnchor GetAnchor(float3 position)
+            {
+                float3 localPosition = WorldToLocal(position);
+
+                float distance = math.abs(PlaneDistance(normal, Missing.up, localPosition));
+
+                localPosition -= normal * distance;
+
+                WallAnchor result;
+
+                result.x = 1.0f - math.saturate(((math.dot(GetOrthogonalLocalSpace(), localPosition) + 1.0f) * 0.5f));
+                result.y = 1.0f - math.saturate((localPosition.y + 1.0f) * 0.5f);
+
+                return result;
+            }
+
+            public WallAnchor UpdateAnchor(WallAnchor anchor, float2 xy)
+            {
+                WallAnchor result;
+
+                result.x = math.saturate(anchor.x - xy.x / GetWidth()); // saturate = clamp (0.0f-1.0f)
+                result.y = math.saturate(anchor.y - xy.y / GetHeight());
+
+                return result;
+            }
+
+            // --- Debug ---
+            public void DebugDraw()
+            {
+                float3 vertex = GetBaseVertex();
+
+                for (int i = 0; i < 4; ++i)
+                {
+                    float3 nextVertex = GetNextVertexCCW(normal, vertex);
+
+                    float3 v0 = transformPoint(center + Missing.mul(size, vertex) * 0.5f);
+                    float3 v1 = transformPoint(center + Missing.mul(size, nextVertex) * 0.5f);
+
+                    Debug.DrawLine(v0, v1, Color.green);
+
+                    vertex = nextVertex;
+                }
+            }
+
+            public void DebugDraw(ref WallAnchor state)
+            {
+                float3 position = GetPosition(state);
+                float3 normal = GetNormalWorldSpace();
+
+                Debug.DrawLine(position, position + normal * 0.3f, Color.red);
+            }
+
+        } // end of wall geometry
 
     }
 }

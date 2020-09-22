00f4b38c03c428484f354a0a4dc0198af4089bb1
Aitor Simona Bouzas <simonaaitor@gmail.com>
2020-09-22 18:16:35 +0200

    starting wall geometry definition, added main attributes, utilities and basic initialization

--- C.W.L.F/Assets/BipedLocomotion/Scripts/Abilities/ClimbingAbility.Wall.cs
+++ C.W.L.F/Assets/BipedLocomotion/Scripts/Abilities/ClimbingAbility.Wall.cs
@@ -12,6 +12,59 @@ namespace CWLF
 
     public partial class ClimbingAbility : SnapshotProvider, Ability
     {
+        public struct WallGeometry
+        {
+            // --- Attributes that define a wall object ---
+            public AffineTransform transform; // Ensure we remain on the same space
+            public float3 scale; // global
+
+            public float3 normal;
+            public float3 center;
+            public float3 size; // local
+
+            // --- Construction ---
+            public static WallGeometry Create()
+            {
+                return new WallGeometry();
+            }
+
+            // --- Create wall geometry from box collider ---
+            public void Initialize(BoxCollider collider, AffineTransform contactTransform)
+            {
+                transform = Convert(collider.transform);
+                scale = collider.transform.lossyScale;
+                center = collider.center;
+                size = collider.size;
+
+                //Initialize(contactTransform);
+            }
+
+            // --- Utility transformation methods ---
+            public float3 transformPoint(float3 point)
+            {
+                // transforms point p from local to world
+                return transform.transform(point * scale);
+            }
+
+            public float3 inverseTransformPoint(float3 point)
+            {
+                // transforms point p from world to local
+                return transform.inverseTransform(point) / scale;
+            }
+
+            public float3 transformDirection(float3 direction)
+            {
+                // transforms direction from local to world
+                return transform.transformDirection(direction);
+            }
+
+            public static AffineTransform Convert(Transform transform)
+            {
+                // Ensure we remain on the same space
+                return new AffineTransform(transform.position, transform.rotation);
+            }
+
+        }
 
     }
 }

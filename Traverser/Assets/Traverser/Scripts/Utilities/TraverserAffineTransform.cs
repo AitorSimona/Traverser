947ce6ab298e0b9f13fc910c5528489aa129d302
Aitor Simona Bouzas <simonaaitor@gmail.com>
2021-02-09 17:18:08 +0100

    Adapted wall object, added methods to custom transform

--- Traverser/Assets/Traverser/Scripts/Utilities/TraverserAffineTransform.cs
+++ Traverser/Assets/Traverser/Scripts/Utilities/TraverserAffineTransform.cs
@@ -1,4 +1,5 @@
 using Unity.Mathematics;
+using UnityEngine;
 
 namespace Traverser
 {
@@ -20,6 +21,27 @@ namespace Traverser
             affineTransform.t = t_;
             affineTransform.q = q_;
             return affineTransform;
+        }
+
+        // transforms point p from local to world                
+        public float3 transform(float3 point)
+        {
+            Matrix4x4 m = Matrix4x4.TRS(t, q, Vector3.one);
+            return m.MultiplyPoint3x4(point);
+        }
+
+        // transforms direction from local to world
+        public float3 transformDirection(float3 direction)
+        {
+            Matrix4x4 m = Matrix4x4.TRS(t, q, Vector3.one);
+            return m.MultiplyVector(direction);
+        }
+
+        // transforms point p from world to local
+        public float3 inverseTransform(float3 point)
+        {
+            Matrix4x4 m = Matrix4x4.TRS(t, q, Vector3.one);
+            return m.inverse.MultiplyPoint3x4(point);
         }
     }
 }

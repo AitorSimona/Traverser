84194702aaaa5191f2cb5f40b43acb3eed7f85a6
Aitor Simona Bouzas <simonaaitor@gmail.com>
2021-02-24 12:13:19 +0100

    Created new TraverserTransition script to hold transition code

---
+++ Traverser/Assets/Traverser/Scripts/Utilities/TraverserTransition.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Traverser
+{
+    public class TraverserTransition : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
